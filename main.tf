dbe68e8fd2f7ad5268a32f7fbdda2a2dd144645f
cpg-qe <142965448+cpg-qe@users.noreply.github.com>
2023-08-24 01:41:38 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,53 @@
+resource "random_id" "server" {
+  byte_length = 8
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "tfeautorgdnd1-${random_id.server.hex}"
+  location = "${var.region}"
+  tags = "${local.common_tags}"
+}
+
+resource "azurerm_app_service_plan" "main" {
+  name                = "${var.prefix}-asp-${random_id.server.hex}"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+  tags = "${local.common_tags}"
+}
+
+resource "azurerm_app_service" "main" {
+  name                = "${var.prefix}-appservice-${random_id.server.hex}"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.main.id}"
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    remote_debugging_enabled = true
+    remote_debugging_version = "VS2017"
+  }
+  tags = "${local.common_tags}"
+}
+
+locals {
+  # Commmon tags to be assigned to all resources
+  common_tags = "${map(
+   " User", "${var.user}",
+   " CostCenter", "${var.cost_center}"
+ )}"
+}
+
+
+output "app_service_name" {
+  value = "${azurerm_app_service.main.name}"
+}
+
+output "app_service_default_hostname" {
+  value = "https://${azurerm_app_service.main.default_site_hostname}"
+}
+
---
+++ variable.tf
@@ -0,0 +1,31 @@
+variable "prefix" {
+  description = "The prefix used for all resources in this example"
+  default = "npr001"
+}
+
+variable "region" {}
+
+
+variable "user" {
+  description = "User requesting the resources"
+  default = "admin"
+}
+
+variable "cost_center" {
+  description = "Cost Center of the User requesting the resources"
+  default = "admin"
+}
+
+variable "subscriptionId" {}
+variable "clientId" {}
+variable "clientSecret" {}
+variable "tenantId" {}
+
+variable "dummy" {
+  default = "dummy"
+}
+
+variable "dummy1" {
+  default = "dummy"
+}
+
